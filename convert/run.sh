c103d04b838666d7691cdf2e7ab31cd7d2f354f6
name-q <qyking@outlook.com>
2025-05-27 18:08:21 +0800

    初始化python环境 使用py虚拟环境 & 忽略虚拟环境文件

---
+++ convert/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
+fi
+python3 video_to_ascii.py
